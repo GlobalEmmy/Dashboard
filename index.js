2eeb8b0674e22624aadd9e04f1fd3ef46e9c709c
Monyem Emmanuel Onuwa <Emmanuel.Monyem@saipem.com>
2023-09-14 11:13:36 +0100

    Added JavaScript to control menu and theme toggle

--- index.js
+++ index.js
@@ -13,3 +13,33 @@ document.onreadystatechange = function() {
                 "body").style.visibility = "visible";
           }
       };
+
+     //====== Side bar Menu Toggle======
+     let sideMenu = document.querySelector("aside");
+     let menuBtn = document.querySelector("#menu-btn");
+     let closeBtn = document.querySelector("#close-btn");
+
+     //Open menu
+     menuBtn.addEventListener("click", () => {
+           sideMenu.style.display = "block"
+     })
+
+     // Close menu
+     closeBtn.addEventListener("click", () => {
+           sideMenu.style.display = "none"
+    })
+
+    // ========Toggling Backgroung Theme and colors ======
+    let themeTogglerDark = document.querySelector(".theme-toggler span:nth-child(2)");
+    let themeTogglerLight = document.querySelector(".theme-toggler span:nth-child(1)");
+
+    themeTogglerDark.addEventListener ("click", () => {
+         document.body.classList.add("dark-theme-variables")
+         themeTogglerDark.classList.add("active")
+         themeTogglerLight.classList.remove("active")
+    })
+    themeTogglerLight.addEventListener ("click", () => {
+          document.body.classList.remove("dark-theme-variables")
+          themeTogglerDark.classList.remove("active")
+          themeTogglerLight.classList.add("active")
+     })
