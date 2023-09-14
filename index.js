63a8a2ba26e3f4f3b7435350b542d5c4531f1215
Monyem Emmanuel Onuwa <Emmanuel.Monyem@saipem.com>
2023-09-14 13:14:17 +0100

    Added JavaScript to populate orders table

--- index.js
+++ index.js
@@ -1,45 +1,66 @@
 /* ========== Loading section ========= */
 
-document.onreadystatechange = function() {
-          if (document.readyState !== "complete") {
-              document.querySelector(
-                "body").style.visibility = "hidden";
-              document.querySelector(
-                "#spinner").style.visibility = "visible";
-          } else {
-              document.querySelector(
-                "#spinner").style.display = "none";
-              document.querySelector(
-                "body").style.visibility = "visible";
-          }
-      };
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector("#spinner").style.visibility = "visible";
+  } else {
+    document.querySelector("#spinner").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+  }
+};
 
-     //====== Side bar Menu Toggle======
-     let sideMenu = document.querySelector("aside");
-     let menuBtn = document.querySelector("#menu-btn");
-     let closeBtn = document.querySelector("#close-btn");
+//====== Side bar Menu Toggle======
+let sideMenu = document.querySelector("aside");
+let menuBtn = document.querySelector("#menu-btn");
+let closeBtn = document.querySelector("#close-btn");
 
-     //Open menu
-     menuBtn.addEventListener("click", () => {
-           sideMenu.style.display = "block"
-     })
+//Open menu
+menuBtn.addEventListener("click", () => {
+  sideMenu.style.display = "block";
+});
 
-     // Close menu
-     closeBtn.addEventListener("click", () => {
-           sideMenu.style.display = "none"
-    })
+// Close menu
+closeBtn.addEventListener("click", () => {
+  sideMenu.style.display = "none";
+});
 
-    // ========Toggling Backgroung Theme and colors ======
-    let themeTogglerDark = document.querySelector(".theme-toggler span:nth-child(2)");
-    let themeTogglerLight = document.querySelector(".theme-toggler span:nth-child(1)");
+// ========Toggling Backgroung Theme and colors ======
+let themeTogglerDark = document.querySelector(
+  ".theme-toggler span:nth-child(2)"
+);
+let themeTogglerLight = document.querySelector(
+  ".theme-toggler span:nth-child(1)"
+);
 
-    themeTogglerDark.addEventListener ("click", () => {
-         document.body.classList.add("dark-theme-variables")
-         themeTogglerDark.classList.add("active")
-         themeTogglerLight.classList.remove("active")
-    })
-    themeTogglerLight.addEventListener ("click", () => {
-          document.body.classList.remove("dark-theme-variables")
-          themeTogglerDark.classList.remove("active")
-          themeTogglerLight.classList.add("active")
-     })
+themeTogglerDark.addEventListener("click", () => {
+  document.body.classList.add("dark-theme-variables");
+  themeTogglerDark.classList.add("active");
+  themeTogglerLight.classList.remove("active");
+});
+themeTogglerLight.addEventListener("click", () => {
+  document.body.classList.remove("dark-theme-variables");
+  themeTogglerDark.classList.remove("active");
+  themeTogglerLight.classList.add("active");
+});
+
+// Fill orders in table
+
+Orders.forEach((order) => {
+  const tr = document.createElement("tr");
+  const trContent = `
+          <td>${order.productName}</td>
+          <td>${order.productNumber}</td>
+          <td>${order.paymentStatus}</td>
+          <td class = "${
+            order.shipping === "Declined"
+              ? "danger"
+              : order.shipping === "pending"
+              ? "warning"
+              : "primary"
+          }">${order.shipping}</td>
+          <td class="primary">Details</td>
+          `;
+  tr.innerHTML = trContent;
+  document.querySelector("table tbody").appendChild(tr);
+});
